de55e593d82f9cd5437f67701e4ee087f012dd38
saramangelo <saramangelo@gmail.com>
2022-11-30 22:31:53 -0800

    added pseudocode to js

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,10 @@
 
 // variables
 
+// var questions
+// var answers
+
+
 // arrays
 
 // Object with properties for questions and answers
@@ -9,5 +13,27 @@
 
 // timer (setInterval function), eventListener for button click
 
-// 
+// reference this to save data:
+
+// function(event) {
+//     event.preventDefault();
+    
+//       // TODO: Create user object from submission
+  
+//     var userObject = {
+//       firstName: firstNameInput.value.trim(),
+//       lastName: lastNameInput.value.trim(),
+//       email: emailInput.value.trim(),
+//       password: passwordInput.value.trim()
+//     };
+
+//     // TODO: Set new submission to local storage 
+  
+//   localStorage.setItem("userObject", JSON.stringify(userObject));
+  
+//   });
+
+
+// init(); // needed to display high scores 
+
 
